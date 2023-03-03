2273892833f0bd84dfc18238f8b14773f2ef747b
zakki <zakkimuzakki050@gmail.com>
2023-03-03 20:46:55 +0700

    jwt done, tinggal middleware dkt lg

--- sdk/config/jwt.go
+++ sdk/config/jwt.go
@@ -1,12 +1,55 @@
 package config
 
 import (
+	"CareerBoost/entity"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var JWT_KEY = []byte("ncjkanasdnmsaojdsaoijsocndinidSNWNWNDWJncinsjdff")
+// type JWTClaim struct {
+// 	Username string
+// 	jwt.RegisteredClaims
+// }
 
-type JWTClaim struct {
-	Username string
-	jwt.RegisteredClaims
+func GenerateToken(payload entity.UserLogin) (string, error) {
+
+	expStr := os.Getenv("JWT_EXP")
+	var exp time.Duration
+	exp, err := time.ParseDuration(expStr)
+	if expStr == "" || err != nil {
+		exp = time.Hour * 1
+	}
+
+	tokenJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, entity.NewUserClaims(payload.Email, exp))
+	tokenJwtReal, err := tokenJWT.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return "", err
+	}
+	return tokenJwtReal, nil
+}
+
+func DecodeToken(signedToken string, ptrClaims jwt.Claims, KEY string) error {
+
+	token, err := jwt.ParseWithClaims(signedToken, ptrClaims, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return "", errors.New("wrong signing method")
+		}
+		return []byte(KEY), nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("token has been tampered with")
+	}
+
+	if !token.Valid {
+		// token is not valid
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
 }
